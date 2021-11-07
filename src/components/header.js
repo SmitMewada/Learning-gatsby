517c31e16000bced2a9566f5be7bde0a9f126ca4
Smit Mewada <smitmewada009@gmail.com>
2021-11-07 15:53:33 +0530

    #2

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,35 @@
-import { Link } from 'gatsby'
-import React from 'react'
+import { Link } from "gatsby"
+import React from "react"
+import * as headerStyles from "./header.module.scss"
 
 const Header = () => {
-    return (
-        <header>
-            <h1>Smit Mewada.</h1>
-            <nav>
-                <ul>
-                    <li> <Link to="/">Home</Link></li>
-                    <li><Link to="/blog">Blog</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                </ul>
-            </nav>
-        </header>
-    )
+  return (
+    <header className={headerStyles.header}>
+      <h1>
+        <Link to="/" className={headerStyles.title}>Smit Mewada.</Link>
+      </h1>
+      <nav>
+        <ul className={headerStyles.navlist}>
+          <li>
+            <Link to="/" className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/blog"className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}>
+              Blog
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact" className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}>Contact</Link>
+          </li>
+          <li>
+            <Link to="/about" className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}>About</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
 }
 
 export default Header
